0f90720da7204c459f82bba2b14101207a296690
18swalsh <18swalsh@gmail.com>
2020-04-07 23:30:11 -0400

    Update wix-worker.js

--- wix-worker.js
+++ wix-worker.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
+   var sleep = Math.random() * 16;
+   if (sleep >= 8) {
+      console.log("working hard...")
+      setTimeout(function(){
+         window.location.reload(1);
+      }, 5000);
+   }
    
-   console.log("working hard...")
+   console.log("hardly working...")
    setTimeout(function(){
-      window.location.reload(1);
-   }, 5000);
+         window.location.reload(1);
+      }, 5000);
    
     Wix.Worker.addEventListener(Wix.Events.PAGE_NAVIGATION, function(page) {
         console.log("Visitor navigated to page" + page.toPage);
