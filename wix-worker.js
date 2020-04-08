ef80ced82d32febbc85c5201c5ca9e7116783285
18swalsh <18swalsh@gmail.com>
2020-04-08 15:09:05 -0400

    Add files via upload

--- wix-worker.js
+++ wix-worker.js
@@ -9,7 +9,42 @@ $(document).ready(function() {
         });
     }
     catch (e){
-        console.log(e);
+        console.log("cannot debug event listener on static site");
+        //console.log(e);
+    }
+
+    var activity =
+        {
+            type:Wix.Activities.Type.ECOMMERCE_CART_ADD
+        };
+
+    var onSuccess = function(d){console.log("Activity ID: " + d.activityId + ", Contact ID: " + d.contactId)};
+    var onFailure = function(d){console.log("Failure message:" + d)};
+
+    try {
+        Wix.Activities.getActivityById(activity, onSuccess, onFailure);
+    }
+    catch (e){
+        console.log("cannot debug getActivityById on static site");
+        //console.log(e);
+    }
+
+    try {
+        var currentMember = Wix.currentMember;
+        console.log("currentMember: " + currentMember);
+    }
+    catch (e){
+        console.log("cannot debug currentMember on static site");
+        //console.log(e);
+    }
+
+    try {
+        var siteMap = Wix.getSiteMap;
+        console.log(siteMap);
+    }
+    catch (e){
+        console.log("cannot debug getSiteMap on static site");
+        //console.log(e);
     }
 
     console.log("hardly working...")
