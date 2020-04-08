0126fe6e7759a94ef0136e2e35a6b1258fe66a96
18swalsh <18swalsh@gmail.com>
2020-04-08 00:27:17 -0400

    Wix object created

--- wix-worker.js
+++ wix-worker.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+    console.log("working hard");
+    console.log(Wix)
+   /*
    var sleep = Math.random() * 16;
    if (sleep >= 8) {
       console.log("working hard...")
@@ -6,13 +9,18 @@ $(document).ready(function() {
          window.location.reload(1);
       }, 5000);
    }
-   
-   console.log("hardly working...")
+
+
    setTimeout(function(){
          window.location.reload(1);
       }, 5000);
-   
-    Wix.Worker.addEventListener(Wix.Events.PAGE_NAVIGATION, function(page) {
-        console.log("Visitor navigated to page" + page.toPage);
+
+   */
+    Wix.onReady(function(){
+        Wix.Worker.addEventListener(Wix.Events.PAGE_NAVIGATION, function(page) {
+            console.log("Visitor navigated to page" + page.toPage);
+        });
     });
+
+    console.log("hardly working...")
 });
