90518da843a5100b129b463c27cb650f41565254
18swalsh <18swalsh@gmail.com>
2020-04-07 22:03:06 -0400

    Update wix-worker.js

--- wix-worker.js
+++ wix-worker.js
@@ -1,3 +1,6 @@
-$(function() {
-   console.log( "ready!" );
-});
+$(document).ready(function() {
+   console.log("working...")
+    Wix.Worker.addEventListener(Wix.Events.PAGE_NAVIGATION, function(page) {
+        console.log("Visitor navigated to page" + page.toPage);
+    });
+  });
